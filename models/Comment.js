3f7586e87d3755e88736362b05b6bebf7bb8dead
Andrea Fang <arhughes43@gmail.com>
2022-07-13 12:08:02 -0700

    validation of pizzas, comments and replies

--- models/Comment.js
+++ models/Comment.js
@@ -8,10 +8,13 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -21,7 +24,6 @@ const ReplySchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
       getters: true
     },
     id: false
@@ -31,10 +33,12 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
